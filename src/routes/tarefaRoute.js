4fbbd4a36113a03b85b410c53a930d7edd49f9a2
nailton-vieira <nailtonvieira@gmail.com>
2025-03-16 14:25:03 -0300

    Enviando atualizações de erros corrigidos nas rotas

--- src/routes/tarefaRoute.js
+++ src/routes/tarefaRoute.js
@@ -3,9 +3,9 @@ const route = require('express').Router();
 const tarefaController = require('../controller/tarefaController.js');
 
 // Definindo as rotas
-route.get('/terefas', tarefaController.getAllTarefas);
+route.get('/tarefas', tarefaController.getAllTarefas);
 route.get('/tarefas/:id', tarefaController.getTarefaById);
-route.post('/terefas', tarefaController.createTarefa);
+route.post('/tarefas', tarefaController.createTarefa);
 route.put('/tarefas/:id', tarefaController.updateTarefa);
 route.delete('/tarefas/:id', tarefaController.deleteTarefa);
 
